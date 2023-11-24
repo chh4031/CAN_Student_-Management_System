a50eaf1c0e0604c932217dad56e16d69995f7ad4
chh4031 <chh4031@gmail.com>
2023-11-25 03:46:18 +0900

    라우터 주석 추가

--- src/router/detailRouter.js
+++ src/router/detailRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const detailRouter = require('../controller/detailController');
 
+// 디테일 페이지 보여주기 라우터
 router.get('/', detailRouter.detailView)
 
 module.exports = router;
--- src/router/mainRouter.js
+++ src/router/mainRouter.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const mainRouter = require('../controller/mainController');
 
+// 메인화면 보여주는 라우터
 router.get('/', mainRouter.mainView)
 
+// 로그인 발동시 라우터
 router.post('/login', mainRouter.mainLogin)
 
 module.exports = router;
