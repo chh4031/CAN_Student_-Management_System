64aa12e6eca4fa7481cb30eda1f15d24901514e4
chh4031 <chh4031@gmail.com>
2023-11-25 03:46:42 +0900

    컨트롤러 주석

--- src/controller/detailController.js
+++ src/controller/detailController.js
@@ -1,8 +1,15 @@
 const useDB = require("../../middleware/db");
 
+let checkButton = 0;
+
 // 로그인 이후 상세화면 보여주기
 const detailView = async(req, res) => {
-    return res.render("detail")
+
+    checkButton = 1;
+    
+    return res.render("detail", {
+        check : checkButton
+    })
 }
 
 module.exports = {detailView}
